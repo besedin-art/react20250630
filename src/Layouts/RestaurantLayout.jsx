93d60dfb2772118c355c922a74bafacdb102c68e
Artem Besedin <besedin.art@gmail.com>
2025-07-28 18:12:53 +0500

    hw-8 fix

--- src/Layouts/RestaurantLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router";
-import { RestaurantContainer } from "../components/Restaurant/RestaurantContainer";
-
-export const RestaurantLayout = () => {
-  const params = useParams();
-  return (
-    <RestaurantContainer id={params.restaurantId} />
-  );
-}
--- src/Layouts/RestaurantsLayout.jsx
+++ src/Layouts/RestaurantsLayout.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router";
 import { useSelector } from "react-redux";
-import { useState } from "react";
 import { selectRestaurantsIds } from "../redux/entities/restaurants/slice";
 import { RestaurantTabContainer } from "../components/RestaurantTab/RestaurantTabContainer";
 import { RestaurantContainer } from "../components/Restaurant/RestaurantContainer";
 
 export const RestaurantsLayout = () => {
   const restaurantsIds = useSelector(selectRestaurantsIds)
-  const [currentRestaurantId, setCurrentRestaurantId] = useState(null);
 
   return (
     <>
@@ -16,14 +14,11 @@ export const RestaurantsLayout = () => {
           <RestaurantTabContainer
             key={id}
             id={id}
-            isActive={id === currentRestaurantId}
-          // onClick={() => setCurrentRestaurantId(id)}
           />
         ))}
       </div>
       <div>
         <Outlet />
-        {/* {currentRestaurantId && <RestaurantContainer id={currentRestaurantId} />} */}
       </div>
       <div style={{ height: "1000px" }}></div>
     </>
